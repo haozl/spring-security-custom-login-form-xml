60bea9c9c10672b2309657f10f39ac0fdf82ee95
vitaminc <hacher@gmail.com>
2015-12-23 01:56:38 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+[![Build Status](https://travis-ci.org/haozl/spring-security-custom-login-form-xml.svg)](https://travis-ci.org/haozl/spring-security-custom-login-form-xml)
+Spring Security Custom Login Form example (Xml Config)
+===============================
+Example on how to configure and create a spring security custom login form with Xml configuration
+
+### 1. Technologies used
+* Spring 4.2.4.RELEASE
+* Spring Security 4.0.3.RELEASE
+* Maven 3
+
+### 2. Run the application
+```shell
+$ git clone [repository]
+$ mvn jetty:run
+```
+Visit ```http://localhost:8080```
+
+### 3. Import the sample project into Eclipse IDE
+1. ```$ mvn eclipse:eclipse```
+2. File->Import->Existing projects into workspace
+
---
+++ src/main/java/com/example/web/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model, Principal principal) {
+        if (principal != null) {
+            String username = principal.getName();
+            model.addAttribute("username", username);
+        }
+        return "hello";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+}
